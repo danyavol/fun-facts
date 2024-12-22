eb587c12964d4e5985e7df04c0b6a5f967c337ea
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2024-12-23 00:04:24 +0100

    feat: Update cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,48 +7,53 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { onValueWritten } from 'firebase-functions/v2/database';
 import { initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
+import { onDocumentWritten } from 'firebase-functions/v2/firestore';
 
 initializeApp();
 
-export const updateTotalFacts = onValueWritten({ ref: '/facts/{factId}', region: 'europe-west1' }, async ({ data }) => {
-    const differences: { quizId: number; difference: number }[] = [];
+export const updateTotalFacts = onDocumentWritten(
+    { document: '/facts/{factId}', region: 'europe-central2' },
+    async ({ data }) => {
+        const differences: { quizId: number; difference: number }[] = [];
 
-    // Find which quiz exactly was changed
-    if (!data.before.exists() && data.after.exists()) {
-        // New fact was created
-        differences.push({
-            quizId: data.after.val().quizId,
-            difference: 1,
-        });
-    } else if (data.before.exists() && !data.after.exists()) {
-        // Fact was deleted
-        differences.push({
-            quizId: data.before.val().quizId,
-            difference: -1,
-        });
-    } else if (data.before.val().quizId !== data.after.val().quizId) {
-        // Quiz ID has changed
-        differences.push({
-            quizId: data.before.val().quizId,
-            difference: -1,
-        });
+        // Find which quiz exactly was changed
+        if (!data?.before.exists && data?.after.exists) {
+            // New fact was created
+            differences.push({
+                quizId: data.after.data()?.quizId,
+                difference: 1,
+            });
+        } else if (data?.before.exists && !data?.after.exists) {
+            // Fact was deleted
+            differences.push({
+                quizId: data.before.data()?.quizId,
+                difference: -1,
+            });
+        } else if (data?.before.data()?.quizId !== data?.after.data()?.quizId) {
+            // Quiz ID has changed
+            differences.push({
+                quizId: data?.before.data()?.quizId,
+                difference: -1,
+            });
 
-        differences.push({
-            quizId: data.after.val().quizId,
-            difference: 1,
-        });
-    } else return;
+            differences.push({
+                quizId: data?.after.data()?.quizId,
+                difference: 1,
+            });
+        } else return;
 
-    await Promise.all(
-        differences.map(async ({ quizId, difference }) => {
-            const quizRef = getFirestore().doc(`/quizzes/${quizId}`);
-            const currentValue = ((await quizRef.get()).data()?.['totalFacts'] as number) ?? 0;
-            const newValue = currentValue + difference;
+        await Promise.all(
+            differences.map(async ({ quizId, difference }) => {
+                if (!quizId) return;
 
-            await quizRef.set({ totalFacts: newValue < 0 ? 0 : newValue });
-        })
-    );
-});
+                const quizRef = getFirestore().doc(`/quizzes/${quizId}`);
+                const currentValue = ((await quizRef.get()).data()?.['totalFacts'] as number) ?? 0;
+                const newValue = currentValue + difference;
+
+                await quizRef.set({ totalFacts: newValue < 0 ? 0 : newValue });
+            })
+        );
+    }
+);
